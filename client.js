d7ba4445fc89cc3671e28db041837193c567bce5
arthursereno <arthurbarbosa2809@gmail.com>
2025-03-30 11:44:57 -0300

    Update client.js

--- client.js
+++ client.js
@@ -1,15 +1,24 @@
+let cooldown = false;
+
 window.TrelloPowerUp.initialize({
   'board-buttons': function(t, options) {
     return [{
       text: '➕ Criar Conta CA',
       callback: function(t) {
+        if (cooldown) {
+          return t.alert({ message: '⏳ Processando...' });
+        }
+
+        cooldown = true; // ativa o "bloqueio"
+        setTimeout(() => { cooldown = false; }, 3000); // libera após 3 segundos
+
         return t.board('id')
           .then(function(board) {
             const webhookUrl = `https://n8n.api-blogfeelgood.fun/webhook/duplicar-conta`;
             return fetch(webhookUrl, {
               method: 'GET'
             })
-            .then(() => t.alert({ message: '✅ Nova conta criada!' }))
+            .then(() => t.alert({ message: '✅ Nova conta criada com sucesso!' }))
             .catch(() => t.alert({ message: '❌ Erro ao criar conta.' }));
           });
       }
